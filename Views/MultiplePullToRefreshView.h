34e044ff71e2c5341d78e92db4cef527a9539df1
yangshengchao <519395228@qq.com>
2014-12-03 16:12:13 +0800

    add multiple pull to refresh view

---
+++ Views/MultiplePullToRefreshView.h
@@ -0,0 +1,18 @@
+//
+//  MultiplePullToRefreshView.h
+//  KQ
+//
+//  Created by yangshengchao on 14/12/3.
+//  Copyright (c) 2014年 yangshengchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/**
+ *  1. 提供一个view里多个下拉刷新的封装
+ *  2. 同时支持UITableView和UICollectionView
+ *  3. 另外还支持多section
+ */
+@interface MultiplePullToRefreshView : UIScrollView
+
+@end
---
+++ Views/MultiplePullToRefreshView.m
@@ -0,0 +1,21 @@
+//
+//  MultiplePullToRefreshView.m
+//  KQ
+//
+//  Created by yangshengchao on 14/12/3.
+//  Copyright (c) 2014年 yangshengchao. All rights reserved.
+//
+
+#import "MultiplePullToRefreshView.h"
+
+@implementation MultiplePullToRefreshView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
