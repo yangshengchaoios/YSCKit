89e584b91e1660e24714b468d71d5f654ae5e0ed
yangshengchao <519395228@qq.com>
2015-03-06 16:57:23 +0800

    添加UIScrollView+Additions

---
+++ Categories/UIScrollView+Additions.h
@@ -0,0 +1,20 @@
+//
+//  UIScrollView+Additions.h
+//  KQ
+//
+//  Created by yangshengchao on 15/3/6.
+//  Copyright (c) 2015年 yangshengchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIScrollView (Additions)
+
+- (BOOL)isAtTop;
+- (BOOL)isAtBottom;
+- (BOOL)isSwipingRight;
+- (BOOL)isSwipingLeft;
+- (BOOL)isSwipingDown;
+- (BOOL)isSwipingUp;
+
+@end
