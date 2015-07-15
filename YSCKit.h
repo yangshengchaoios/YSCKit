729d6f9b27a6de3b90b515fd70a5e92c57967e64
yangshengchao <yangshengchaoios@gmail.com>
2015-07-15 22:53:06 +0800

    add new header

---
+++ YSCKit.h
@@ -0,0 +1,13 @@
+//
+//  YSCKit.h
+//  EZGoal
+//
+//  Created by yangshengchao on 15/7/15.
+//  Copyright (c) 2015年 Builder. All rights reserved.
+//
+
+#ifndef EZGoal_YSCKit_h
+#define EZGoal_YSCKit_h
+
+
+#endif
